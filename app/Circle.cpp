2d418933460dcc5004e349918809810226068784
bsaisudh <bsaisudh@terpmail.umd.edu>
2018-10-12 02:52:52 -0400

    [STUB] created stub for Circle class

---
+++ app/Circle.cpp
@@ -0,0 +1,32 @@
+/*
+ * Circle.cpp
+ *
+ *  Created on: Oct 12, 2018
+ *      Author: bala
+ */
+
+#include "../include/Circle.h"
+
+Circle::Circle()
+    : center(0),
+      radius(0) {
+  // TODO Auto-generated constructor stub
+
+}
+
+void Circle::setBoundary() {
+
+}
+
+void Circle::dispBoundary() {
+
+}
+
+bool Circle::inObstacle(int coordX, int coordY) {
+  return false;
+}
+
+Circle::~Circle() {
+  // TODO Auto-generated destructor stub
+}
+
---
+++ include/Circle.h
@@ -0,0 +1,25 @@
+/*
+ * Circle.h
+ *
+ *  Created on: Oct 12, 2018
+ *      Author: bala
+ */
+
+#ifndef APP_CIRCLE_H_
+#define APP_CIRCLE_H_
+
+#include "Obstacle.h"
+
+class Circle : public Obstacle {
+ private:
+  int center;
+  int radius;
+ public:
+  Circle();
+  void setBoundary();
+  void dispBoundary();
+  bool inObstacle(int xCoord, int yCoord);
+  virtual ~Circle();
+};
+
+#endif /* APP_CIRCLE_H_ */
