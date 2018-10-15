5219c492a4c66498789f2721dba6a2ccb20b87f5
bsaisudh <bsaisudh@terpmail.umd.edu>
2018-10-15 15:36:54 -0400

    [UTEST] written unit test for following classes -Obstacle -Square -Circle -RobotWorkSapce

--- test/ObstacleTest.cpp
+++ test/ObstacleTest.cpp
@@ -3,7 +3,7 @@
  * @Copyright MIT license
  * Copyright (c) 2018 Bala Murali Manoghar Sai Sudhakar
  * @author: Bala Murali Manoghar Sai Sudhakar
- *
+ * @brief testing obstacle and its derived class
  */
 
 #include <gtest/gtest.h>
@@ -22,8 +22,8 @@ struct SquareTest : public testing::Test {
   Square *sq;
   void SetUp() {
     sq = new Square;
-    std::stringstream sin;
     std::stringstream sout;
+    std::stringstream sin;
     sin.str("");
     sout.str("");
     sin << "2 2 2 ";
@@ -40,11 +40,11 @@ struct SquareTest : public testing::Test {
  * @brief Testing square initialization
  */
 TEST(SquareClass, Initialization) {
-  //Assert
+  // Assert
   Square square;
-  //Act
-  std::stringstream sin;
   std::stringstream sout;
+  std::stringstream sin;
+  // Act
   sin.str("");
   sout.str("");
   sin << "2 2 2 ";
@@ -52,7 +52,7 @@ TEST(SquareClass, Initialization) {
   sin.str("");
   sout.str("");
   square.dispBoundary(sout);
-//  Test
+  // Test
   ASSERT_STREQ(sout.str().c_str(), "StartX:2 StartY :2 Side: 2\n");
 }
 
@@ -66,14 +66,14 @@ TEST_F(SquareTest , insideOutsideObstacle) {
 
 
 /**
- * @brief Square class test fixture
+ * @brief Circle class test fixture
  */
 struct CircleTest : public testing::Test {
   Circle *cir;
   void SetUp() {
     cir = new Circle;
-    std::stringstream sin;
     std::stringstream sout;
+    std::stringstream sin;
     sin.str("");
     sout.str("");
     sin << "2 2 2 ";
@@ -90,11 +90,11 @@ struct CircleTest : public testing::Test {
  * @brief Testing circle initialization
  */
 TEST(CircleClass, Initialization) {
-  //Assert
+  // Assert
   Circle circle;
-  //Act
-  std::stringstream sin;
   std::stringstream sout;
+  std::stringstream sin;
+  // Act
   sin.str("");
   sout.str("");
   sin << "2 2 2 ";
@@ -102,15 +102,113 @@ TEST(CircleClass, Initialization) {
   sin.str("");
   sout.str("");
   circle.dispBoundary(sout);
-//  Test
+  // Test
   ASSERT_STREQ(sout.str().c_str(), "Center X :2 Center Y :2 Radius :2\n");
 }
 
 /**
- * @brief Testing if point is inside square obstacle or not
+ * @brief Testing if point is inside circle obstacle or not
  */
 TEST_F(CircleTest , insideOutsideObstacle) {
   ASSERT_EQ(cir->inObstacle(10,10),false);
   ASSERT_EQ(cir->inObstacle(3,3),true);
+}
+
+/**
+ * @brief Obstacle Circle class test fixture for overriding
+ */
+struct ObstacleCircleTest : public testing::Test {
+  Obstacle *ob_cir;
+  void SetUp() {
+    ob_cir = new Circle;
+    std::stringstream sout;
+    std::stringstream sin;
+    sin.str("");
+    sout.str("");
+    sin << "2 2 2 ";
+    ob_cir->setBoundary(sin, sout);
+    sin.str("");
+    sout.str("");
+  }
+  void TearDown() {
+    delete ob_cir;
+  }
+};
+
+/**
+ * @brief Testing circle initialization fir overriding
+ */
+TEST(ObstacleCircleClass, Initialization) {
+  // Assert
+  Obstacle *ob_circle(new Circle);
+  std::stringstream sout;
+  std::stringstream sin;
+  // Act
+  sin.str("");
+  sout.str("");
+  sin << "2 2 2 ";
+  ob_circle->setBoundary(sin, sout);
+  sin.str("");
+  sout.str("");
+  ob_circle->dispBoundary(sout);
+  // Test
+  ASSERT_STREQ(sout.str().c_str(), "Center X :2 Center Y :2 Radius :2\n");
+}
+
+/**
+ * @brief Testing if point is inside circle obstacle or not through overriding
+ */
+TEST_F(ObstacleCircleTest , insideOutsideObstacle) {
+  ASSERT_EQ(ob_cir->inObstacle(10,10),false);
+  ASSERT_EQ(ob_cir->inObstacle(3,3),true);
+}
+
+/**
+ * @brief Obstacle square class test fixture for overriding
+ */
+struct ObstacleSquareTest : public testing::Test {
+  Obstacle *ob_sq;
+  void SetUp() {
+    ob_sq = new Square;
+    std::stringstream sout;
+    std::stringstream sin;
+    sin.str("");
+    sout.str("");
+    sin << "2 2 2 ";
+    ob_sq->setBoundary(sin, sout);
+    sin.str("");
+    sout.str("");
+  }
+  void TearDown() {
+    delete ob_sq;
+  }
+};
+
+/**
+ * @brief Testing square class initialization for overriding
+ */
+TEST(ObstaclesquareClass, Initialization) {
+  // Assert
+  Obstacle *ob_square(new Square);
+  std::stringstream sout;
+  std::stringstream sin;
+  // Act
+  sin.str("");
+  sout.str("");
+  sin << "2 2 2 ";
+  ob_square->setBoundary(sin, sout);
+  sin.str("");
+  sout.str("");
+  ob_square->dispBoundary(sout);
+  // Test
+  ASSERT_STREQ(sout.str().c_str(), "StartX:2 StartY :2 Side: 2\n");
+}
+
+/**
+ * @brief Testing if point is inside circle obstacle or not through overriding
+ */
+TEST_F(ObstacleSquareTest , insideOutsideObstacle) {
+  ASSERT_EQ(ob_sq->inObstacle(10,10),false);
+  ASSERT_EQ(ob_sq->inObstacle(3,3),true);
 }
 
---
+++ test/RobotWorkspaceTest.cpp
@@ -0,0 +1,76 @@
+/*
+ * @file RobotWorkspaceTest.cpp
+ * @Copyright MIT license
+ * Copyright (c) 2018 Bala Murali Manoghar Sai Sudhakar
+ * @author: Bala Murali Manoghar Sai Sudhakar
+ * @brief Testing RobotWorksapce class
+ */
+
+#include <gtest/gtest.h>
+#include <sstream>
+#include <iostream>
+#include <string>
+
+#include "../include/RobotWorkspace.h"
+
+/**
+ * @brief RobotWorkspace class test fixture
+ */
+struct RobotWorkspaceTest : public testing::Test {
+  RobotWorkspace *ws;
+  void SetUp() {
+    ws = new RobotWorkspace;
+    std::stringstream sout;
+    std::stringstream sin;
+    sin.str("");
+    sout.str("");
+    sin << "10 10 9 9 2 2 ";
+    ws->setBoundary(sin, sout);
+    ws->setGoal(sin, sout);
+    ws->setStart(sin, sout);
+    sin.str("");
+    sout.str("");
+  }
+  void TearDown() {
+    delete ws;
+  }
+};
+
+/**
+ * @brief Testing RobotWorkspace initialization
+ */
+TEST(RobotWorkSpaceClass, Initialization) {
+  // Assert
+  RobotWorkspace robotWs;
+  std::stringstream sout;
+  std::stringstream sin;
+  // Act
+  sin.str("");
+  sout.str("");
+  sin << "10 10 9 9 2 2 ";
+  robotWs.setBoundary(sin, sout);
+  robotWs.setGoal(sin, sout);
+  robotWs.setStart(sin, sout);
+  sin.str("");
+  sout.str("");
+  robotWs.dispWorkslace(sout);
+  // Test
+  ASSERT_STREQ(
+      sout.str().c_str(),
+      "Start point :2 , 2\nGoal point :9 , 9\nWorkspace : ( 0 , 0 ) ( 10 , 10)\n");
+}
+
+/**
+ * @brief Testing if point is inside square obstacle or not
+ */
+
+TEST_F(RobotWorkspaceTest , ComputeAndGetMethods) {
+  ASSERT_EQ(ws->inWorkspace(5,5), true);
+  ASSERT_EQ(ws->inWorkspace(115,115), false);
+  ASSERT_EQ(ws->isGoal(9,9), true);
+  ASSERT_EQ(ws->isGoal(7,7), false);
+  ASSERT_EQ(ws->isStart(2,2), true);
+  ASSERT_EQ(ws->isStart(3,3), false);
+  ASSERT_EQ(ws->getmaxX(), 10);
+  ASSERT_EQ(ws->getmaxY(), 10);
+}
