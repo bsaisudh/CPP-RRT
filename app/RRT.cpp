dd3d93fe0c754fc32f8b7a6d3a15345e51bfd642
bsaisudh <bsaisudh@terpmail.umd.edu>
2018-10-16 23:08:58 -0400

    [INTEG] integrating RRt Algorithm

--- app/RRT.cpp
+++ app/RRT.cpp
@@ -1,8 +1,9 @@
 /*
- * RRT.cpp
- *  Copyright Akshay Rajaraman and Bala Sai Sudhakar
- *  Created on: Oct 12, 2018
- *      Author: bala
+ * @file RRT.cpp
+ * @Copyright MIT license
+ * Copyright (c) 2018 Akshay Rajaraman, Bala Murali Manoghar Sai Sudhakar
+ * @author Akshay Rajaraman, Bala Murali Manoghar
+ * @brief RRT algorithm implementation class
  */
 
 #include "../include/RRT.h"
@@ -27,30 +28,29 @@ bool RRT::isGoal() {
 }
 
 void RRT::sampleFromCs() {
+  // @brief seeding random sampler
   std::srand(std::time(nullptr));
+  /* @brief obtaining the location of a random point
+   * from the configuration space
+   */
   int random_location = std::rand()/((RAND_MAX + 1u)/Map -> configSpace.size());
   sampledPoint[0] = Map -> configSpace.at(random_location).x;
   sampledPoint[1] = Map -> configSpace.at(random_location).y;
 }
 
 void RRT::addToTree() {
-    int parentPointIndex = computeNewPoint();
-    point point;
-    point.x = newPoint[0];
-    point.y = newPoint[1];
-    for (int iter = 0; iter < Map -> configSpace.size(); iter++) {
-      if (Map -> configSpace.at(iter).x == point.x &&
-          Map -> configSpace.at(iter).y == point.y) {
-        int test = Tree.size();
-        Tree.push_back({{newPoint[0], newPoint[1],
-                static_cast<int>(Tree.size())+1,
-                Tree.at(parentPointIndex)[2]}});
-      }
-    }
+    parentPointIndex = computeNewPoint();
+    int test = Tree.size();
+    // @brief adding new point to Tree
+    Tree.push_back({{newPoint[0], newPoint[1],
+                test,
+                parentPointIndex}});
 }
 
 int RRT::computeNewPoint() {
-  std::vector<double> distance;
+    /* @brief Locating the index of the closest point on the Tree from the
+     sampled point*/
+    std::vector<double> distance;
     for (int i = 0; i < Tree.size(); i++) {
       distance.push_back(std::pow(std::pow(Tree.at(i)[0]-sampledPoint[0], 2)+
           std::pow(Tree.at(i)[1]-sampledPoint[1], 2), 0.5));
@@ -63,15 +63,18 @@ int RRT::computeNewPoint() {
          index = i;
       }
     }
-    int availableMovements[8][2] = {{Tree.at(index)[1], Tree.at(index)[2]+1},
-        {Tree.at(index)[1]+1, Tree.at(index)[2]+1},
-        {Tree.at(index)[1]+1, Tree.at(index)[2]},
-        {Tree.at(index)[1]+1, Tree.at(index)[2]-1},
-        {Tree.at(index)[1], Tree.at(index)[2]-1},
-        {Tree.at(index)[1]-1, Tree.at(index)[2]-1},
-        {Tree.at(index)[1]-1, Tree.at(index)[2]},
-        {Tree.at(index)[1]-1, Tree.at(index)[2]+1}};
+    /* @brief Determining the possible movements from the
+      point on the tree through 8 point connectivity*/
+    int availableMovements[8][2] = {{Tree.at(index)[0], Tree.at(index)[1]+1},
+        {Tree.at(index)[0]+1, Tree.at(index)[1]+1},
+        {Tree.at(index)[0]+1, Tree.at(index)[1]},
+        {Tree.at(index)[0]+1, Tree.at(index)[1]-1},
+        {Tree.at(index)[0], Tree.at(index)[1]-1},
+        {Tree.at(index)[0]-1, Tree.at(index)[1]-1},
+        {Tree.at(index)[0]-1, Tree.at(index)[1]},
+        {Tree.at(index)[0]-1, Tree.at(index)[1]+1}};
     std::vector<double> distanceFromNewPoints;
+    // @brief obtaining index of closest point from 8 points
     for (int j = 0; j < 8; j++) {
       distanceFromNewPoints.push_back(std::pow(
           std::pow(availableMovements[j][0]-sampledPoint[0], 2)+
@@ -87,10 +90,12 @@ int RRT::computeNewPoint() {
       }
       newPoint[0] = availableMovements[newPointIndex][0];
       newPoint[1] = availableMovements[newPointIndex][1];
+      // @brief returning parent point index
       return index;
 }
 
 bool RRT::updateSampleSpace() {
+  // @brief removing used sample point from configuration space
   point erasePoint;
   erasePoint.x = sampledPoint[0];
   erasePoint.y = sampledPoint[1];
@@ -98,6 +103,7 @@ bool RRT::updateSampleSpace() {
         if (Map -> configSpace.at(iter).x == erasePoint.x &&
             Map -> configSpace.at(iter).y == erasePoint.y) {
           Map ->  configSpace.erase(Map -> configSpace.begin()+iter);
+          break;
         }
       }
   if (Map -> configSpace.size() == 1) {
@@ -108,46 +114,39 @@ bool RRT::updateSampleSpace() {
 }
 
 std::vector<point> RRT::buildPath() {
-  /*
+  // @brief Obtaining start and goal points from input map
   startPoint[0] = Map -> ws -> startX;
   startPoint[1] = Map -> ws -> startY;
   goalPoint[0] = Map -> ws -> goalX;
   goalPoint[1] = Map -> ws -> goalY;
-  Tree.push_back({{startPoint[0], startPoint[1], 1, 0}});
+  // @brief Adding start point to tree
+  Tree.push_back({{startPoint[0], startPoint[1], 0, 0}});
   while (!isGoal()) {
     sampleFromCs();
     addToTree();
-    computeNewPoint();
     bool isNotReachable = updateSampleSpace();
     if (isNotReachable) {
         break;
       }
   }
+  Tree.push_back({{goalPoint[0], goalPoint[1],
+                  static_cast<int>(Tree.size()),
+                  parentPointIndex}});
   std::vector<point> path;
   int newIndex = Tree.size() - 1;
   point parentPoint;
-  while (newIndex != 0) {
+  // @brief obtain path coordinates
+  while (newIndex > 0) {
     parentPoint.x = Tree.at(newIndex)[0];
     parentPoint.y = Tree.at(newIndex)[1];
     path.push_back(parentPoint);
     newIndex = Tree.at(newIndex)[3];
-    std::cout << Tree.at(newIndex)[0];
-    std::cout << Tree.at(newIndex)[1];
-    std::cout << Tree.at(newIndex)[2];
-    std::cout << Tree.at(newIndex)[3];
-    int x;
-    std::cin >> x;
-    std::cout << newIndex;
-  }*/
-  std::vector<point> path;
-  point point;
-  point.x = 2;
-  point.y = 3;
-  path.emplace_back(point);
+  }
+  parentPoint.x = Tree.at(0)[0];
+  parentPoint.y = Tree.at(0)[1];
+  path.push_back(parentPoint);
   return path;
 }
 
 RRT::~RRT() {
 }
-
-
